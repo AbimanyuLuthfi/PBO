22050d2cba7d6ab7882f9d36dd1e931b6924427d
AbimanyuLuthfi <89892701+AbimanyuLuthfi@users.noreply.github.com>
2021-10-21 09:55:30 +0700

    Create InputanSoal4.java

---
+++ UTS_PBO/JAWABAN_SOAL_NO4/InputanSoal4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class InputanSoal4 {
+    
+    protected double r;
+    protected int pi;
+    protected double d;
+    
+    public void setr (double r) {
+        this.r = r;
+    }
+    public void setd (double d) {
+        this.d = d;
+    }
+    public void setpi (int p) {
+        this.pi = p;
+    }
+
+    public int getpi () {
+    return this.pi;
+    }
+    public double getr () {
+    return this.r;
+    }
+    public double getd () {
+    return this.d;
+    }
+    
+}
